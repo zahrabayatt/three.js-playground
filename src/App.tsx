5a0586a71c7e5615f4b5338809cf32a4a5928c5c
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-12-10 11:25:11 +0330

    Create react project using vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SpinningCube from "./components/SpinningCube";
+
+function App() {
+  return (
+    <div>
+      <SpinningCube />
+    </div>
+  );
+}
+
+export default App;
